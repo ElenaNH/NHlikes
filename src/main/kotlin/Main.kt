8fd9056aebc5444b4bdc3018e4b79df60c50044a
Elena Nefedova <elenanefedova@gmail.com>
2022-08-30 00:16:44 +0300

    Changed LIKES to PEOPLE

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,14 @@
 fun main(args: Array<String>) {
-    val likes: Int = 9
-
+    val likes: Int = 520
     /*println("(likes % 10)=${likes % 10}")
     println("(likes / 10)=${likes / 10}")
     println("((likes / 10) % 10)=${(likes / 10) % 10}")*/
 
     val word: String = if ((likes % 10 == 1) && ((likes / 10) % 10 != 1)) {
-        "лайк"
-    } else if ((likes % 10 >= 1) && (likes % 10 <= 4) && ((likes / 10) % 10 != 1)) {
-        "лайка"
+        "человеку"
     } else {
-        "лайков"
+        "людям"
     }
-    println("У вас $likes $word")
+    println("Понравилось $likes $word")
 
 }
